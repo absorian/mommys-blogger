542bd7410e57a52605e1d4bdcc5cceb4be1d950f
absorian <07221432bs@gmail.com>
2023-07-22 23:34:28 +0300

    add blog preview

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ export * from "./firebase"
 import Box from "./Box.svelte"
 import Button from "./Button.svelte"
 import ColumnTable from "./ColumnTable.svelte"
+import BlogPreview from "./BlogPreview.svelte"
 
-export { Box, Button, ColumnTable }
+export { Box, Button, ColumnTable, BlogPreview }
