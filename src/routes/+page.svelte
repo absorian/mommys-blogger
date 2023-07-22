79773921fcb8594bd340511d453d48e7ddd5d962
absorian <07221432bs@gmail.com>
2023-07-22 19:51:40 +0300

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
-    import { Button } from "$lib"    
+	import { Box } from '$lib';
+	import { Heading, Text, YoutubeEmbed, ImageBox } from '$lib/BlogItems';
+
+	let editing = false;
 </script>
 
+<Box>
+	<Heading {editing} text="Hello lorem" />
+	<Text
+		{editing}
+		text="Lorem ipsum dolor sit amet consectetur adipisicing elit. A, perferendis saepe. Pariatur perferendis soluta provident necessitatibus officiis cum nesciunt consectetur vero! Excepturi autem a nostrum accusantium accusamus provident perspiciatis debitis?"
+	/>
+	<YoutubeEmbed {editing} url="https://www.youtube.com/watch?v=D9-voINFkCg" />
+	<Heading {editing} text="Hello lorem" />
+	<ImageBox
+		{editing}
+		id="https://www.subhe.com/img/clips/INPUTTYPE2(19)722.jpg"
+		src="https://www.subhe.com/img/clips/INPUTTYPE2(19)722.jpg"
+		alt="test"
+		desc="Test lorem"
+	/>
+</Box>
+
+<style>
+</style>
